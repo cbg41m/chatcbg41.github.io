cc4a0385a841f92ccfcfd74ea81421b2374dc6a8
cbg41m <81341309+cbg41m@users.noreply.github.com>
2021-03-24 22:43:51 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDH0idD0srO8T81FjI5ErO1LpWTwvWu4f0",
+   apiKey: "AIzaSyDH0idD0srO8T81FjI5ErO1LpWTwvWu4f0",
     authDomain: "chatcbg41.firebaseapp.com",
     projectId: "chatcbg41",
     storageBucket: "chatcbg41.appspot.com",
     messagingSenderId: "600922779707",
-    appId: "1:600922779707:web:105b22e44b28f12a5a2c1e",
-    measurementId: "G-2BD56VCDWF"
+    appId: "1:600922779707:web:ccada4f50a3b22305a2c1e",
+    measurementId: "G-710YJEFM61"
     });
